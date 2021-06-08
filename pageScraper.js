73283f168f3add39e100104a06d78a87ab0cf4e5
Sergey Belokrylets <s.belokrylets@gmail.com>
2021-06-08 18:11:55 +0300

    fix: изменение pageScraper.js

--- pageScraper.js
+++ pageScraper.js
@@ -4,6 +4,34 @@ const scraperObject = {
         let page = await browser.newPage();
         console.log(`Переход на ${this.url}...`);
         await page.goto(this.url);
+        await page.waitForSelector('.items-items-38oUm');
+       
+        let urls = await page.$$eval('.iva-item-content-m2FiN', links => {
+            links = links.map(el => el.querySelector('div > a').href)
+            return links;        
+        });
+
+           
+           let pagePromise = (link) => new Promise(async(resolve, reject) => {
+            let dataObj = {};
+            let newPage = await browser.newPage();
+            await newPage.goto(link);
+            dataObj['title'] = await newPage.$eval('.title-info-title > span', text => text.textContent);
+            dataObj['description'] = await newPage.$eval('.item-description-text > p', text => text.textContent);
+            dataObj['url'] = link;
+            dataObj['price'] = await newPage.$eval('.price-value-string > span', text => text.textContent);
+            dataObj['author'] = await newPage.$eval('.sticky-header-prop > span', text => text.textContent);
+            dataObj['date'] = await newPage.$eval('.title-info-metadata-item-redesign', text => text.textContent);
+            
+            
+            resolve(dataObj);
+            await newPage.close();
+        });
+
+        for(link in urls){
+            let currentPageData = await pagePromise(urls[link]);
+            console.log(currentPageData);
+        }
 
     }
 }
