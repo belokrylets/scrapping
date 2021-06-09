f5f510ba06e005eb2df7cbd99536393d6f28a6a3
Sergey Belokrylets <s.belokrylets@gmail.com>
2021-06-09 10:36:37 +0300

    fix: стандартизированна дата

--- pageScraper.js
+++ pageScraper.js
@@ -22,7 +22,40 @@ const scraperObject = {
             dataObj['url'] = link;
             dataObj['price'] = await newPage.$eval('.price-value-string', price => Number(price.textContent.trim().replace(/[^\d]+/g,""))) 
             dataObj['author'] = await newPage.$eval('.sticky-header-prop > span', author => author.textContent);
-            dataObj['date'] = await newPage.$eval('.title-info-metadata-item-redesign', date => date.textContent.trim());
+            dataObj['date'] = await newPage.$eval('.title-info-metadata-item-redesign', dates => {
+                const date = dates.textContent.trim()
+                const mapping = {
+                    января: '01',
+                    февраля: '02',
+                    марта: '03',
+                    апреля: '04',
+                    мая: '05',
+                    июня: '06',
+                    июля: '07',
+                    августа: '08',
+                    сентября: '09',
+                    октября: '10',
+                    ноября: '11',
+                    декабря: '12',
+                  }
+                    if (date.includes('сегодня')) {
+                      const today = new Date();
+                      const day = String(today.getDate()).padStart(2, '0');
+                      const month = String(today.getMonth() + 1).padStart(2, '0');
+                      return `2021-${month}-${day}`;
+                    };
+                    if (date.includes('вчера')) {
+                      const yesterday = new Date();
+                      yesterday.setDate(yesterday.getDate() - 1);
+                      const day = String(yesterday.getDate()).padStart(2, '0');
+                      const month = String(yesterday.getMonth() + 1).padStart(2, '0');
+                      return `2021-${month}-${day}`;
+                    };
+                    const arrowDate = date.split(' ');
+                    const month = mapping[arrowDate[1]];
+                    const day = arrowDate[0].padStart(2, '0');
+                    return `2021-${month}-${day}`;
+            });
             
             
             resolve(dataObj);
